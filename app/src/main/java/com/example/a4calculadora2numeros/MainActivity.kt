994e742320e9681904db2ad0129c8e72b7ab3e6c
SergioAlmagre <127857356+SergioAlmagre@users.noreply.github.com>
2023-09-18 00:02:02 +0200

    Updates

--- app/src/main/java/com/example/a4calculadora2numeros/MainActivity.kt
+++ app/src/main/java/com/example/a4calculadora2numeros/MainActivity.kt
@@ -2,10 +2,77 @@ package com.example.a4calculadora2numeros
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var res:Int
+
+        var btSum = findViewById<Button>(R.id.btSum)
+        var btSub = findViewById<Button>(R.id.btSubtraction)
+        var btMul = findViewById<Button>(R.id.btMultipli)
+        var btSpl = findViewById<Button>(R.id.btSplit)
+        var btCle = findViewById<Button>(R.id.btClean)
+        var n1 = findViewById<EditText>(R.id.n1)
+        var n2 = findViewById<EditText>(R.id.n2)
+        var result = findViewById<TextView>(R.id.idResult)
+
+        btSum.setOnClickListener{
+            if(areCorrect(n1,n2)){
+                res = n1.text.toString().toInt() +  n2.text.toString().toInt()
+                result.setText(res.toString())
+            }
+        }
+
+        btSub.setOnClickListener{
+            if(areCorrect(n1,n2)) {
+                res = n1.text.toString().toInt() - n2.text.toString().toInt()
+                result.setText(res.toString())
+            }
+        }
+
+        btMul.setOnClickListener{
+            if(areCorrect(n1,n2)){
+            res = n1.text.toString().toInt() *  n2.text.toString().toInt()
+            result.setText(res.toString())
+            }
+        }
+
+        btSpl.setOnClickListener {
+            if (areCorrect(n1, n2)) {
+            res = n1.text.toString().toInt() / n2.text.toString().toInt()
+            result.setText(res.toString())
+            }
+        }
+
+        btCle.setOnClickListener{
+            n1.setText("")
+            n2.setText("")
+            result.setText("0")
+        }
+
     }
+
+    fun areCorrect(n1:EditText, n2:EditText):Boolean{
+        var areCorrect = false
+        try{
+            var num1 = n1.toString().toInt()
+            var num2 = n2.toString().toInt()
+        }catch(e:Exception){
+            Log.e("Sergio","${e}")
+        }
+        return areCorrect
+    }
+
+
+
+
+
+
+
 }
